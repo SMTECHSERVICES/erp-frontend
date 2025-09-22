cacc736f30401e7d1adb72e2964427b928076641
SMTECHSERVICES <abhishekmishara59@gmail.com>
2025-09-22 17:35:27 +0530

    add be server

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,6 @@
 
 
 // export const server = import.meta.env.VITE_ENVIRONMENT_MODE === "production" ? "" : "http://localhost:3000/api"
+export const server = "https://erp-server-ic51.onrender.com"
 
-export const server = "http://localhost:3000/api"
+//export const server = "http://localhost:3000/api"
